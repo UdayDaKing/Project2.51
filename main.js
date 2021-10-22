2d2d7a18ee274b60ac96e4b2ccf68ae947ddf05a
UdayDaKing <88062206+UdayDaKing@users.noreply.github.com>
2021-10-21 20:22:35 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function StartClassification(){
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/BJiqHvf90/model.json',modelReady);
+}
+function modelReady(){
+    classifier.classify(gotResults);
+}
